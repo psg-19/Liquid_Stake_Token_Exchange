0d29d6929024d7e3a31ad5d201816ee0bbd89c26
psg-19 <pratushgupt22@gmail.com>
2024-12-21 04:03:07 +0530

    fixing cors

--- server/index.js
+++ server/index.js
@@ -24,13 +24,13 @@ require('dotenv').config();
 app.use(express.json());
 
 
-app.use(cors({
-  origin: "*",
-//   origin: FRONTEND_URL,
+// app.use(cors({
+//   origin: "*",
+// //   origin: FRONTEND_URL,
 
-  optionsSuccessStatus: 200,
-  credentials: true 
-}));
+//   optionsSuccessStatus: 200,
+//   credentials: true 
+// }));
 
 
 function getDaysSince() 
